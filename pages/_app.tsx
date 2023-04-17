bf30cfbee29ac23c08d846162fd5a48d2e5e15c4
Aboubacar Diall <a.diallo3012@gmail.com>
2023-04-17 08:08:32 +0200

    update redux persist config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "tailwindcss/tailwind.css"
 import "../styles/globals.css"
 import Header from "../components/Header"
-
+import type { AppProps } from "next/app"
 // redux
-import { store } from '../redux/store'
-import { Provider } from 'react-redux'
+import { store } from "../redux/store"
+import { Provider } from "react-redux"
 import { persistStore } from "redux-persist"
 import { PersistGate } from "redux-persist/integration/react"
 
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   const persistor = persistStore(store)
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate persistor={persistor} loading={null}>
         <Header />
         <hr />
         <Component {...pageProps} />
