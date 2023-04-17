f3c710ec63051413c336dadafff529a5dd8729cf
Aboubacar Diall <a.diallo3012@gmail.com>
2023-04-17 20:26:41 +0200

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 const navigation = {
   main: [
     { name: "Next.js", href: "https://nextjs.org/" },
@@ -52,16 +54,10 @@ export default function Footer() {
         </div>
         <hr />
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-          <nav
-            className="-mx-5 -my-2 flex flex-wrap justify-center"
-            aria-label="Footer"
-          >
+          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.main.map((item) => (
               <div key={item.name} className="px-5 py-2">
-                <Link
-                  href={item.href}
-                  className="text-base text-gray-500 hover:text-gray-900"
-                >
+                <Link href={item.href} className="text-base text-gray-500 hover:text-gray-900">
                   {item.name}
                 </Link>
               </div>
