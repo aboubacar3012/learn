d88e86ac9e8062dde4cc536534dd76a6b5a59c45
Aboubacar Diallo <a.diallo3012@gmail.com>
2023-04-30 23:36:27 +0200

    update inscription page

--- pages/inscription.tsx
+++ pages/inscription.tsx
@@ -82,7 +82,7 @@ const SignUp = () => {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="bg-white w-full md:max-w-md lg:max-w-full  md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+        <div className="bg-white w-full md:max-w-md lg:max-w-full  md:mx-0 md:w-1/2 xl:w-1/3 md:h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
           <div className="w-full h-100">
             <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12 text-center md:text-left">
               Inscription
